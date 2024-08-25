ba550e414800b6ffd22e635a7f2224b1abb15b0c
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-08-26 01:59:27 +0530

    Time: 19 ms (68.7%), Space: 28.3 MB (9.9%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,35 +3,24 @@ public:
     void rotate(vector<int>& nums, int k) {
         
         int size = nums.size();
-        int x = 0;
+        int x = 0,idx;
         vector<int>vec(size);
         if(size > k){
-            int idx = size - k;
-
-            for(int i = idx; i < size; i++){
-                vec[x] = nums[i];
-                x++;
-            }
-
-            for(int i = 0; i < idx; i++){
-                vec[x] = nums[i];
-                x++;
-            }
-            
+            idx = size - k;
             
         }else{
 
-            int idx = size - (k%size);
-            for(int i = idx; i < size; i++){
+            idx = size - (k%size);
+        }
+
+        for(int i = idx; i < size; i++){
                 vec[x] = nums[i];
                 x++;
             }
 
-            for(int i = 0; i < idx; i++){
-                vec[x] = nums[i];
-                x++;
-            }
-
+        for(int i = 0; i < idx; i++){
+            vec[x] = nums[i];
+            x++;
         }
 
         nums = vec;
