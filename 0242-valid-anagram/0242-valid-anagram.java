c56554c3b1b696b660bf25aa9f9bde1406ee9bde
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-09-02 00:53:03 +0530

    Time: 9 ms (34.57%), Space: 44.7 MB (28.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char w1[] = new char[s.length()];
-        char w2[] = new char[t.length()];
+        char w1[] = s.toCharArray();
+        char w2[] = t.toCharArray();;
 
-        for(int i = 0 ; i < s.length() ; i++){
-            w1[i] = s.charAt(i);
-        }
+        // for(int i = 0 ; i < s.length() ; i++){
+        //     w1[i] = s.charAt(i);
+        // }
         
-        for(int i = 0 ; i < t.length() ; i++){
-            w2[i] = t.charAt(i);
-        }
+        // for(int i = 0 ; i < t.length() ; i++){
+        //     w2[i] = t.charAt(i);
+        // }
 
         Arrays.sort(w1);
         Arrays.sort(w2);
