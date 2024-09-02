b2ba8d649865ff991b896c8ae1099c66adb1239b
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-09-03 00:26:09 +0530

    Time: 2 ms (72.59%), Space: 60.1 MB (31.16%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,17 +1,59 @@
+// class Solution {
+//     public int chalkReplacer(int[] chalk, int k) {
+        
+//         // int sum = 0;
+//         // int index = 0;
+//         // int ans;
+//         // while(sum <= k){
+//         //     if(index >= chalk.length){
+//         //         index = 0;
+//         //     }
+//         //     sum += chalk[index];
+//         //     index = index + 1;
+//         // }
+
+//         // return index-1;
+
+//         int total = 0;
+//         for(int c: chalk){
+//             total += c;
+//         }
+
+//         k %= total;
+
+//         for(int i = 0 ; i < chalk.length ; i++){
+
+//             if(k < chalk[i]){
+//                 return i;
+//             }
+//             k -= chalk[i];
+//         }
+
+//         return -1;
+//     }
+// }
+
+
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        
-        int sum = 0;
-        int index = 0;
-        int ans;
-        while(sum <= k){
-            if(index >= chalk.length){
-                index = 0;
-            }
-            sum += chalk[index];
-            index = index + 1;
+        // Calculate the total sum of chalk
+        long totalChalk = 0;
+        for (int c : chalk) {
+            totalChalk += c;
         }
 
-        return index-1;
+        // Reduce k by the total chalk sum using modulo
+        k %= totalChalk;
+
+        // Find the index of the student who will replace the chalk
+        for (int i = 0; i < chalk.length; i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+
+        // Should not reach here as the problem guarantees a solution
+        return -1;
     }
 }
