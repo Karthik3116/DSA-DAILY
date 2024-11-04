363b3a4edf524773ffc7d9dab2deed620f08909d
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-04 20:37:53 +0530

    Time: 2 ms (98.83%), Space: 44.8 MB (46.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,47 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        // int count;
-        // int []ans = new int[2];
-        // for(int i = 0 ; i < nums.length; i++){
-        //     for(int j = i+1 ; j < nums.length ; j++){
-        //         if((nums[i]+nums[j]) == target){
-        //             ans[0] = i;
-        //             ans[1] = j;
+        HashMap<Integer, Integer> mymap = new HashMap<>();
 
-        //             break;
-        //         }
-        //     }
-        // }
-
-        // int []retval = new int[2];
-        // HashMap<Integer , Integer > answer = new HashMap<>();
-        // for(int i = 0 ; i < nums.length ; i++){
-
-        //     if(answer.containsKey(nums[i])){
-        //         retval[0] = answer.get(nums[i]);
-        //         retval[1] = i;
-        //         break;
-        //     }else{
-        //         answer.put(target-nums[i], i);
-        //     }
-        // }
-        // return retval;
-
-        HashMap<Integer,Integer> answer = new HashMap<>();
-        int []retval = new int[2];
-
-        for(int i = 0 ; i < nums.length; i++){
-
-            if(answer.containsKey(nums[i])){
-                retval[0] = answer.get(nums[i]);
-                retval[1] = i;
-                break;
+        for(int i = 0 ; i < nums.length ; i++){
+            
+            if(mymap.containsKey(nums[i])){
+                return new int[] {mymap.get(nums[i]) , i};
             }else{
-                answer.put(target-nums[i],i);
+                mymap.put((target - nums[i]), i);
             }
         }
-        return retval;
-        // return ans;
+
+        return new int[] {};
     }
 }
