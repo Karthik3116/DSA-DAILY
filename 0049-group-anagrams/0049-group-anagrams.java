e318358ff9fb0456c39240daa58525ebd9ff1cd5
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-04 23:00:31 +0530

    Time: 18 ms (22.36%), Space: 49.2 MB (6.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,32 +1,55 @@
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> mymap = new HashMap<>();
-        for(String one : strs){
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         HashMap<String,List<String>> mymap = new HashMap<>();
+//         for(String one : strs){
 
             
-            char []arr = one.toCharArray();
+//             char []arr = one.toCharArray();
 
-            Arrays.sort(arr);
-            String str = Arrays.toString(arr);
-            if(mymap.containsKey(str)){
+//             Arrays.sort(arr);
+//             String str = Arrays.toString(arr);
+//             if(mymap.containsKey(str)){
                     
-                List<String> temp = new ArrayList<>(mymap.get(str));
-                temp.add(one);
-                mymap.put(str ,temp );
+//                 List<String> temp = new ArrayList<>(mymap.get(str));
+//                 temp.add(one);
+//                 mymap.put(str ,temp );
 
-            }else{
+//             }else{
 
-                List<String> temp = new ArrayList<>();
-                temp.add(one);
-                mymap.put(str ,temp );
+//                 List<String> temp = new ArrayList<>();
+//                 temp.add(one);
+//                 mymap.put(str ,temp );
+//             }
+//         }
+
+//         List<List<String>> ans = new ArrayList<>();
+
+//         for(Map.Entry<String, List<String>> entry : mymap.entrySet()){
+//             ans.add(entry.getValue());
+//         }
+//         return ans;
+//     }
+// }
+
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       
+       HashMap<String , List<String>> mymap = new HashMap<>();
+
+        for(String s : strs){
+            int []arr = new int[26];
+            for(char ch : s.toCharArray()){
+                arr[ch - 'a'] += 1;
             }
+
+            String key = Arrays.toString(arr);
+            mymap.putIfAbsent(key , new ArrayList<>());
+            mymap.get(key).add(s);
         }
 
-        List<List<String>> ans = new ArrayList<>();
+        return new ArrayList<>(mymap.values());
 
-        for(Map.Entry<String, List<String>> entry : mymap.entrySet()){
-            ans.add(entry.getValue());
-        }
-        return ans;
+
     }
 }
