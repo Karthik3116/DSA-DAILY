e5f229ce6b430666e50ecd94a301f13d0e99f7b9
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-04 14:36:25 +0530

    Time: 8 ms (36.42%), Space: 42.3 MB (99.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,13 +3,30 @@ class Solution {
         
         
 
-        char []onec = s.toCharArray();
-        char []twoc = t.toCharArray();
-        Arrays.sort(onec);
-        Arrays.sort(twoc);
+        // char []onec = s.toCharArray();
+        // char []twoc = t.toCharArray();
+        // Arrays.sort(onec);
+        // Arrays.sort(twoc);
         
         
-        return Arrays.equals(onec , twoc);
+        // return Arrays.equals(onec , twoc);
+        if(s.length() != t.length()) return false;
+        int []arr = new int[26];
+
+        for(int i = 0 ; i < s.length() ; i++){
+
+            arr[s.charAt(i) - 'a'] = arr[s.charAt(i) - 'a'] + 1;
+            arr[t.charAt(i) - 'a'] = arr[t.charAt(i) - 'a'] - 1;
+             
+        }
+
+        for(int num : arr){
+            if(num != 0){
+                return false;
+            }
+        }
+
+        return true;
          
     }
 }
