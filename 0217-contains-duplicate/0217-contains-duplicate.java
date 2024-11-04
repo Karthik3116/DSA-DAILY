13609126fe79d5a9b5c02d90f2d700e651f72629
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-04 14:00:49 +0530

    Time: 2 ms (99.82%), Space: 55 MB (93.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,35 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> mymap = new HashSet<>();
+
+//         for(int num : nums){
+//             if(!mymap.add(num)){
+//                 return true;
+//             }
+//         }
+
+//         return false;
+
+//     }
+// }
+
+
+import java.util.Arrays;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> mymap = new HashSet<>();
-
-        for(int num : nums){
-            if(!mymap.add(num)){
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] == nums[i])
                 return true;
+            else if (nums[i] < nums[i - 1]){
+                int temp = nums[i - 1];
+                for(int j = i - 2; j >= 0; j--){
+                    if(nums[i] == nums[j])
+                        return true;
+                }
+                nums[i - 1] = nums[i];
+                nums[i] = temp;
             }
         }
-
         return false;
 
     }
