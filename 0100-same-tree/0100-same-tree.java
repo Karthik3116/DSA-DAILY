679ec5eaa08a936a0d050535a7d37a65962afae0
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-12 23:44:51 +0530

    Time: 0 ms (100%), Space: 41.2 MB (21.24%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,44 +13,44 @@
  *     }
  * }
  */
-// class Solution {
-//     public boolean isSameTree(TreeNode p, TreeNode q) {
-//         Queue<TreeNode> qu = new LinkedList<>();
-//         qu.add(p);
-//         qu.add(q);
-        
-
-//         while(!qu.isEmpty()){
-//             TreeNode node1 = qu.poll();
-//             TreeNode node2 = qu.poll();
-            
-//             if(node1 == null && node2 == null) continue;
-
-//             if(node1 == null || node2 == null || node1.val != node2.val) return false;
-
-//             qu.add(node1.left);
-//             qu.add(node2.left);
-//             qu.add(node1.right);
-//             qu.add(node2.right);
-            
-            
-//         }
-
-//         return true;
-//     }
-// }
-
-
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(p);
+        qu.add(q);
+        
+
+        while(!qu.isEmpty()){
+            TreeNode node1 = qu.poll();
+            TreeNode node2 = qu.poll();
+            
+            if(node1 == null && node2 == null) continue;
+
+            if(node1 == null || node2 == null || node1.val != node2.val) return false;
+
+            qu.add(node1.left);
+            qu.add(node2.left);
+            qu.add(node1.right);
+            qu.add(node2.right);
+            
+            
         }
 
-        if(p == null || q == null || (p.val != q.val)){
-            return false;
-        }
-
-        return isSameTree(p.left ,q.left) && isSameTree(p.right , q.right );
+        return true;
     }
 }
+
+
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p == null && q == null){
+//             return true;
+//         }
+
+//         if(p == null || q == null || (p.val != q.val)){
+//             return false;
+//         }
+
+//         return isSameTree(p.left ,q.left) && isSameTree(p.right , q.right );
+//     }
+// }
