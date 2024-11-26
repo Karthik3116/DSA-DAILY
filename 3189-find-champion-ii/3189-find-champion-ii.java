8b252bbee6d7be5f9fd1c138877f1b85dd25be89
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-26 20:43:31 +0530

    Time: 1 ms (100%), Space: 45.7 MB (61.76%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -4,7 +4,7 @@ class Solution {
     public int findChampion(int n, int[][] edges) {
         int[] incoming = new int[n];
         int count = n;
-        System.out.println("//////"+count  +  " " + Arrays.toString(incoming));
+        
         for(int[] row : edges){
             if(incoming[row[1]] == 0){
                 count -= 1;
@@ -12,7 +12,7 @@ class Solution {
             incoming[row[1]]+=1;
         }
         
-        System.out.println(count  +  " " + Arrays.toString(incoming));
+        
         if(count == 1){
             for(int i = 0 ; i < n ; i++){
                 if(incoming[i] == 0){
