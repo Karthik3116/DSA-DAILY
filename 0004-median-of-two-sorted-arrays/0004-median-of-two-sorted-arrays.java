fc6308322370242ce78105e7f7dffb5604e4c0c1
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-27 00:31:16 +0530

    Time: 1 ms (100%), Space: 46.3 MB (13.07%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,10 +1,10 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        int[] nums = new int[nums1.length + nums2.length];
+        int[] nums = new int[nums1.length + nums2.length] ;
 
 
-        int i = 0, j = 0, k = 0;
+        int i = 0, j = 0, k = 0 , count = 0;
         int mid = nums.length / 2;
         boolean even = nums.length % 2 == 0;
 
@@ -15,25 +15,60 @@ class Solution {
             } else {
                 nums[k++] = nums2[j++];
             }
+            count += 1;
+
+            if(count > mid){
+                if (even) {
+            
+                    return (nums[mid] + nums[mid - 1]) / 2.0;
+                } else {
+                    
+                    return nums[mid];
+                }
+            }
         }
 
 
         while (i < nums1.length) {
             nums[k++] = nums1[i++];
+            count += 1;
+
+            if(count > mid){
+                if (even) {
+            
+                    return (nums[mid] + nums[mid - 1]) / 2.0;
+                } else {
+                    
+                    return nums[mid];
+                }
+            }
         }
 
 
         while (j < nums2.length) {
             nums[k++] = nums2[j++];
+            count += 1;
+
+            if(count > mid){
+                if (even) {
+            
+                    return (nums[mid] + nums[mid - 1]) / 2.0;
+                } else {
+                    
+                    return nums[mid];
+                }
+            }
         }
 
 
-        if (even) {
+        // if (even) {
             
-            return (nums[mid] + nums[mid - 1]) / 2.0;
-        } else {
+        //     return (nums[mid] + nums[mid - 1]) / 2.0;
+        // } else {
             
-            return nums[mid];
-        }
+        //     return nums[mid];
+        // }
+
+        return -1;
     }
 }
