6886542bae8c242d0df107772096e008fd3910c5
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-27 00:21:50 +0530

    Time: 1 ms (100%), Space: 46.3 MB (13.07%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,27 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Bruteforce
-        int [] nums = new int[nums1.length + nums2.length  ];
+        
+        int[] nums = new int[nums1.length + nums2.length];
 
-        for(int i = 0 ; i < nums1.length  ; i++){
-            nums[i] = nums1[i];
+
+        int i = 0, j = 0, k = 0;
+        int mid = nums.length / 2;
+        boolean even = nums.length % 2 == 0;
+
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                nums[k++] = nums1[i++];
+            } else {
+                nums[k++] = nums2[j++];
+            }
         }
-        int j = 0;
-        for(int i = nums1.length ; i < nums1.length + nums2.length  ; i++){
-            nums[i] = nums2[j];
-            j+=1;
+
+
+        while (i < nums1.length) {
+            nums[k++] = nums1[i++];
         }
 
-        Arrays.sort(nums);
 
-        if(nums.length % 2 == 0){
-            int one = nums[nums.length/2] , two = nums[(nums.length/2) - 1];
-            System.out.println(one + " " + two);
-            return (double) (one + two) / 2 ;
+        while (j < nums2.length) {
+            nums[k++] = nums2[j++];
+        }
 
-        }else{
-            return (double) nums[nums.length / 2];
+
+        if (even) {
             
+            return (nums[mid] + nums[mid - 1]) / 2.0;
+        } else {
+            
+            return nums[mid];
         }
     }
 }
