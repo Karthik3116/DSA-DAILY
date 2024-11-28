5aaa6df80d364196044518c884a21d6587e06bb6
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-11-29 02:42:31 +0530

    Time: 0 ms (100%), Space: 43.4 MB (6.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,26 @@
 class Solution {
+    public double calc(double x , int n){
+        if(n == 0){return 1;};
+        if(x == 0){return 0;};
+
+        double temp = calc(x , n/2);
+
+        if(n%2 == 0){
+            return temp * temp;
+        }
+
+        else{
+            return x * temp * temp;
+        }
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        
+        if(n < 0){
+            x = 1/x;
+            n = -n;
+        }
+
+        return calc(x , n);
+
     }
 }
