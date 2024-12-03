0d31109a7c1fa6fae71f184a5550c883321b86ad
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-03 16:02:04 +0530

    Time: 18 ms (91.44%), Space: 55 MB (99.38%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -44,50 +44,8 @@ class Solution {
     }
 
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+        // quickSort(nums, 0, nums.length - 1);
+        Arrays.sort(nums);
         return nums;
     }
 }
-
-
-
-
-
-// class Solution {
-
-//     public int partition(int []arr ,int low , int high ){
-//         int pivot = arr[high];
-//         int i = low - 1;
-
-//         for(int j = low ; j < high ; j++){
-//             if(arr[j] < pivot){
-//                 i++;
-//                 int temp = arr[i];
-//                 arr[i] = arr[j];
-//                 arr[j] = temp;
-//             }
-//         }
-//         i++;
-//         int temp = arr[i];
-//         arr[i] = pivot;
-//         arr[high] = temp; 
-//         return i;
-//     }
-
-//     public void Quick_sort(int []arr , int low , int high){
-
-//         if(low < high){
-//             int pidx = partition(arr , low , high);
-//             Quick_sort(arr, low , pidx - 1);
-//             Quick_sort(arr, pidx + 1 , high);
-            
-//         }
-//     }
-
-//     public int[] sortArray(int[] nums) {
-
-//         Quick_sort(nums, 0, nums.length - 1);
-//         return nums;
-
-//     }
-// }
