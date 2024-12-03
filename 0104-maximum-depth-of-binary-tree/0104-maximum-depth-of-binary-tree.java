616558b662b2e576375ba4d6b199506a1d69b70f
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-03 15:09:22 +0530

    Time: 0 ms (100%), Space: 42.7 MB (29.62%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,47 +13,47 @@
  *     }
  * }
  */
-// class Solution {
-//     public int maxDepth(TreeNode root) {
-//         if(root == null){return 0;};
-        
-//         int left =  maxDepth(root.left);
-//         int right =  maxDepth(root.right);
-        
-//         return Math.max(left , right) + 1;
-//     }   
-// }
-
-
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null){return 0;};
-        Queue<TreeNode> q = new LinkedList<>();
-
-        q.add(root);
-        int ans = 1;
-        while(!q.isEmpty()){
-
-            int n = q.size();
-            
-            for(int i = 0 ; i < n ; i++){
-                TreeNode node = q.poll();
-
-                if(node.left != null){
-                    q.add(node.left);
-                }
-                if(node.right != null){
-                    q.add(node.right);
-                }
-                
-            }
-            if(!q.isEmpty()){
-                ans++;
-            }
-
-
-        }
-
-        return ans;
+        
+        int left =  maxDepth(root.left);
+        int right =  maxDepth(root.right);
+        
+        return Math.max(left , right) + 1;
     }   
 }
+
+
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if(root == null){return 0;};
+//         Queue<TreeNode> q = new LinkedList<>();
+
+//         q.add(root);
+//         int ans = 1;
+//         while(!q.isEmpty()){
+
+//             int n = q.size();
+            
+//             for(int i = 0 ; i < n ; i++){
+//                 TreeNode node = q.poll();
+
+//                 if(node.left != null){
+//                     q.add(node.left);
+//                 }
+//                 if(node.right != null){
+//                     q.add(node.right);
+//                 }
+                
+//             }
+//             if(!q.isEmpty()){
+//                 ans++;
+//             }
+
+
+//         }
+
+//         return ans;
+//     }   
+// }
