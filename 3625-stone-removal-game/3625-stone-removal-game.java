993322ee3234e9218e692fe6056bc29bd304fee3
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-04 23:17:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3625-stone-removal-game/3625-stone-removal-game.java
+++ 3625-stone-removal-game/3625-stone-removal-game.java
@@ -1,15 +1,25 @@
+// class Solution {
+//     public boolean canAliceWin(int n) {
+
+//         int round = 1, removed = 0;
+//         int curr = 10;
+        
+//         while(removed <= n){
+//             removed += curr;
+//             curr--;
+//             round++;
+//         }
+
+//         return round%2 != 0 ?  true:  false;
+//     }
+// }
+
 class Solution {
     public boolean canAliceWin(int n) {
+        // Solve the quadratic equation to find the number of rounds
+        int k = (int) Math.ceil((21 - Math.sqrt(21 * 21 - 8 * n)) / 2);
 
-        int round = 1, removed = 0;
-        int curr = 10;
-        
-        while(removed <= n){
-            removed += curr;
-            curr--;
-            round++;
-        }
-
-        return round%2 != 0 ?  true:  false;
+        // Determine the winner based on whether the round is odd or even
+        return k % 2 == 0; // True if odd, Alice wins
     }
 }
