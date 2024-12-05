53f6bd1fbb67a8a6d82089fbcb9ae904a588738a
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-05 09:20:21 +0530

    Time: 1 ms (93.43%), Space: 41.7 MB (11.97%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         int match = 0;
-        if(s.length() == 0 && t.length() > 0){return true;};
+        if(s.length() == 0){return true;};
+        if(t.length() == 0){return false;};
         
-        if(s.length() == 0 && t.length() == 0){return true;};
         for(char curr : t.toCharArray()){
-            if(curr == s.charAt(match) && match <= s.length()){
+            if(curr == s.charAt(match)){
                 match++;
             }
             if(match == s.length()){
