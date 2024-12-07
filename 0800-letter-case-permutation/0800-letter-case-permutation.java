f24190fcde6c4a52bb29e44c0591a3e5d9788375
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-07 21:32:49 +0530

    Time: 7 ms (35.06%), Space: 46.1 MB (6.88%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public List<String> solve(String input, String output, List<String> ans, int index) {
+        if (index == input.length()) {
+            ans.add(output);
+            return ans;
+        }
+
+        char currentChar = input.charAt(index);
+
+        if (!Character.isLetter(currentChar)) {
+            return solve(input, output + currentChar, ans, index + 1);
+        }
+
+        solve(input, output + currentChar, ans, index + 1);
+
+        // Include the character in its opposite case
+        if (Character.isUpperCase(currentChar)) {
+            solve(input, output + Character.toLowerCase(currentChar), ans, index + 1);
+        } else {
+            solve(input, output + Character.toUpperCase(currentChar), ans, index + 1);
+        }
+
+        return ans;
+    }
+
+    public List<String> letterCasePermutation(String s) {
+        List<String> ans = new ArrayList<>();
+        return solve(s, "", ans, 0);
+    }
+}
