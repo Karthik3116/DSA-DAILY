49be5d173548357c63b60fa1ad5736a1f80925b0
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-09 02:57:23 +0530

    Time: 2 ms (48.98%), Space: 43.7 MB (15.95%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,43 +1,68 @@
 class Solution {
-    public static boolean check(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if ((c == ')' && top != '(') || 
-                    (c == ']' && top != '[') || 
-                    (c == '}' && top != '{')) {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
+    // public static boolean check(String s) {
+    //     Stack<Character> stack = new Stack<>();
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(' || c == '[' || c == '{') {
+    //             stack.push(c);
+    //         } else {
+    //             if (stack.isEmpty()) {
+    //                 return false;
+    //             }
+    //             char top = stack.pop();
+    //             if ((c == ')' && top != '(') || 
+    //                 (c == ']' && top != '[') || 
+    //                 (c == '}' && top != '{')) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return stack.isEmpty();
+    // }
 
 
-    public List<String> print(String output , int inputlen ,List<String> ans){
-        if(output.length() == inputlen){
+    // public List<String> print(String output , int inputlen ,List<String> ans){
+    //     if(output.length() == inputlen){
 
-            if(check(output)){
-                ans.add(output);
-                // System.out.println(output);
-            }
+    //         if(check(output)){
+    //             ans.add(output);
+    //             System.out.println(output);
+    //         }
+    //         return ans;
+    //     }
+
+    //     print(output + "(" , inputlen ,ans);
+    //     print(output + ")" , inputlen, ans);
+    //     return ans;
+        
+    // }
+
+    public List<String> solve(List<String> ans , int open , int close , int total , String output){
+        if(open+close == total){
+            ans.add(output);
             return ans;
         }
 
-        print(output + "(" , inputlen ,ans);
-        print(output + ")" , inputlen, ans);
-        return ans;
         
+        if(close > open ){
+            return ans ;
+        }
+
+        if(open == total/2){
+            return solve(ans , open , close+1 , total , output + ")");
+        }
+        if(close == total / 2){
+            return solve(ans , open + 1 , close , total , output + "(");
+        }
+        solve(ans , open + 1, close , total , output + "(");
+        solve(ans , open , close+1 , total , output + ")");
+
+        return ans;
     }
+    
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        ans = print(new String("") , 2*n, ans);
+        // ans = print(new String("") , 2*n, ans);
+        ans = solve(ans , 1,0,n*2 , new String("(")); 
         return ans;
     }
 }
