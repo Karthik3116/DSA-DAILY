566607431356de8bd85b533417b1f00f0bbec870
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-12 21:32:21 +0530

    Time: 4 ms (51.14%), Space: 48.4 MB (99.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -8,14 +8,14 @@ class Pair{
 }
 class Solution {
 
-    public void bfs(char[][]grid , boolean[][]visited , int row , int col){
+    public void bfs(char[][]grid  , int row , int col){
 
         int m = grid.length;
         int n = grid[0].length;
 
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(row , col));
-
+        grid[row][col] = '0';
         int[]row_dir = {1,-1,0,0};
         int[]col_dir = {0,0,1,-1};
 
@@ -29,9 +29,10 @@ class Solution {
                 int new_row = node_row - row_dir[k];
                 int new_col = node_col - col_dir[k];
 
-                if(new_row >= 0 && new_row < m && new_col >= 0 && new_col < n && !visited[new_row][new_col] && grid[new_row][new_col] == '1'){
+                if(new_row >= 0 && new_row < m && new_col >= 0 && new_col < n && grid[new_row][new_col] == '1'){
                     q.add(new Pair(new_row,new_col));
-                    visited[new_row][new_col] = true;
+                    grid[new_row][new_col] = '0';
+
                 }
                 
             }
@@ -41,15 +42,12 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
-        boolean [][]visited = new boolean[m][n];
-
         int count = 0;
 
         for(int i = 0 ; i < m; i++){
             for(int j = 0 ; j < n ; j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
-                    bfs(grid , visited , i , j);
+                if( grid[i][j] == '1'){
+                    bfs(grid  , i , j);
                     count++;
                 }
             }
