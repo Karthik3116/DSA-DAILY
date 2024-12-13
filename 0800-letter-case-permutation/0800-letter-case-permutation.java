50b0afc5bb745da2b79a58d163f89115230db1f8
karthik <127582465+Karthik3116@users.noreply.github.com>
2024-12-13 21:26:56 +0530

    Time: 8 ms (24.93%), Space: 45.5 MB (49.49%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.java
+++ 0800-letter-case-permutation/0800-letter-case-permutation.java
@@ -1,31 +1,28 @@
-
 class Solution {
 
-    public List<String> solve(String input , String output , List<String> ans , int curr_index){
+    public List<String> solve(String input , String output , List<String> ans , int curr_index ){
         if(curr_index == input.length()){
+
             ans.add(output);
             return ans;
         }
-
-        char curr_char = input.charAt(curr_index);
-        if(!Character.isLetter(curr_char)){
-            return solve(input , output + curr_char, ans , curr_index + 1);
+        while( curr_index < input.length() && !Character.isLetter(input.charAt(curr_index)) ){
+            output += input.charAt(curr_index);
+            curr_index++;
         }
 
-        // solve(input , output + curr_char, ans , curr_index + 1);
-
-        // if(Character.isUpperCase(curr_char)){
-            solve(input , output + Character.toLowerCase(input.charAt(curr_index)),ans, curr_index +1 );
-        // }else{
-            solve(input , output + Character.toUpperCase(input.charAt(curr_index)),ans, curr_index +1 );    
-        // }
+        if (curr_index == input.length()) {
+            ans.add(output);
+            return ans;
+        }
+        
+        solve(input , output + Character.toLowerCase(input.charAt(curr_index)) ,ans ,curr_index+1  );
+        solve(input , output + Character.toUpperCase(input.charAt(curr_index)) ,ans ,curr_index+1  );
 
         return ans;
-
     }
-
     public List<String> letterCasePermutation(String s) {
-        List<String> ans = new ArrayList<>();
-        return solve(s, "", ans, 0);
+        
+        return solve(s , "" , new ArrayList<String>() , 0);
     }
 }
