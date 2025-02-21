193640a919bafd3b4df79e2ee0ec0a4130448520
karthik <127582465+Karthik3116@users.noreply.github.com>
2025-02-21 22:23:31 +0530

    Time: 3 ms (22.84%), Space: 43 MB (99.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,36 @@
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+        
+//         int c = 0;
+//         for(int i = 1 ; i < nums.length ; i++){
+
+//             if(nums[c] != 0){
+//                 c++;
+//             }
+
+//             if(nums[i] != 0 && c < i){
+//                 int temp = nums[i];
+//                 nums[i] = nums[c];
+//                 nums[c] = temp;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int count = 0;
-        
-
+        int c = 0;
         for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] ==  0){
-                count += 1;
-            }else{
+            if(nums[i] != 0){
                 int temp = nums[i];
-                nums[i] = nums[i- count];
-                nums[i- count] = temp;
+                nums[i] = nums[c];
+                nums[c] = temp;
+                c++;
             }
         }
+        System.gc();
+
     }
 }
