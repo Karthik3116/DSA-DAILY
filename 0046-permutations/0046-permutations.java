62de1f40d1daad46fdfebaa10f241f0c792aa18a
karthik <127582465+Karthik3116@users.noreply.github.com>
2025-03-01 11:54:45 +0530

    Time: 1 ms (92.95%), Space: 44.2 MB (98.26%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,89 +3,38 @@
 
 
 
-// class Solution {
-    
-//     public void solve(int[] input,boolean[] visited ,   List<List<Integer>> ans , List<Integer> output){
-
-//         if(output.size() == input.length){
-//             ans.add(new ArrayList(output));
-//             return;
-//         }
-
-//         for(int i = 0 ; i < input.length ; i++){
-
-//             if(!visited[i]){
-//                 visited[i] = true;
-//                 output.add(input[i]);
-//                 solve(input , visited , ans , output);
-//                 output.remove(output.size() -1);
-//                 visited[i] = false;
-
-//             }
-//         }
-//     }
-
-//     public List<List<Integer>> permute(int[] nums) {
-//         List<List<Integer>> ans = new ArrayList<>();
-//         solve(nums, new boolean[nums.length], ans, new ArrayList<>());   
-//         return ans;
-
-//     }
-// }
-
-
-
-
-
-
-
-
 class Solution {
-    public void solve(int[] input , int[] output , boolean[] visited , List<List<Integer>> ans, int idx){
+    
+    public void solve(int[] input,  List<List<Integer>> ans , List<Integer> output){
 
-        if(idx == input.length){
-            Integer[] temp = Arrays.stream(output).boxed().toArray(Integer[]::new);
-            ans.add(Arrays.asList(temp));
-            return ;
+        if(output.size() == input.length){
+            ans.add(new ArrayList(output));
+            return;
         }
 
         for(int i = 0 ; i < input.length ; i++){
 
-            if(!visited[i]){
-                visited[i] = true;
-                output[idx] = input[i];
-                solve(input , output , visited , ans , idx+1);
-                visited[i] = false;
-                
+            if(input[i] != -11){
+               
+               
+                output.add(input[i]);
+                input[i] = -11;
+                solve(input , ans , output);
+                input[i] = output.get(output.size()-1);
+
+                output.remove(output.size() -1);
+               
             }
         }
     }
+
     public List<List<Integer>> permute(int[] nums) {
-        
-        
-        
         List<List<Integer>> ans = new ArrayList<>();
-        
-        solve(nums , new int[nums.length] , new boolean[nums.length] ,ans ,0);
+        solve(nums, ans, new ArrayList<>());   
         return ans;
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
