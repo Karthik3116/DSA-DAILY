0f428f37d0333cb67eef1d142cfddba7c29bf8e6
karthik <127582465+Karthik3116@users.noreply.github.com>
2025-04-23 14:57:08 +0530

    Time: 232 ms (5.02%), Space: 45.3 MB (6.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -10,14 +10,20 @@ class Solution {
             return ;
         }
 
+
         ans.add(input[idx]);
+        System.out.println(ans);
         solve(input , idx , target - input[idx] , ans , output);
         ans.remove(ans.size() - 1);
         solve(input , idx + 1 , target , ans , output);
+        
+
+        
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
         
         solve(candidates , 0 , target, new ArrayList<>() , ans);
         return ans;
